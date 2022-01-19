ce4d89554b1f74489061a13574b759e04f4ea58d
wafforyu <wafforyu@gmail.com>
2022-01-19 18:08:24 +0800

    move app from lab-js to here

---
+++ app.js
@@ -0,0 +1,11 @@
+import geocode from "./utils/geocode.js";
+import forecast from "./utils/forecast.js";
+
+geocode("Quezon City Philippines", (error, data) => {
+  console.log(error ?? data);
+
+  forecast(data.lat, data.lon, (error, data) => {
+    console.log("Error", error);
+    console.log("Data", data);
+  });
+});
