0c65dfb9bfb43525ff8cd13ef3441143e9af5402
wafforyu <wafforyu@gmail.com>
2022-01-22 19:53:24 +0800

    add address checker

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import geocode from "./utils/geocode.js";
 import forecast from "./utils/forecast.js";
 
-geocode(process.argv[2], (error, { lat, lon, loc }) => {
-  console.log(error ?? lat, lon, loc);
-  forecast(lat, lon, (error, forecastData) => {
-    console.log(error ?? `Location: ${loc}\n${forecastData}`);
+const address = process.argv[2];
+
+if (!address) {
+  console.log("Please specify address");
+} else {
+  geocode(address, (error, { lat, lon, loc }) => {
+    console.log(error ?? lat, lon, loc);
+    forecast(lat, lon, (error, forecastData) => {
+      console.log(error ?? `Location: ${loc}\n${forecastData}`);
+    });
   });
-});
+}
