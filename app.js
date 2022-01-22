2f3c11c0b2c351d1e3846967236bb5c70c0911f7
wafforyu <wafforyu@gmail.com>
2022-01-22 18:54:03 +0800

    print loc

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import forecast from "./utils/forecast.js";
 
 geocode("Quezon City Philippines", (error, data) => {
   console.log(error ?? data);
-  forecast(data.lat, data.lon, (error, data) => {
-    console.log(error ?? data);
+  forecast(data.lat, data.lon, (error, forecastData) => {
+    console.log(error ?? `Location: ${data.loc}\n${forecastData}`);
   });
 });
