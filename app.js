06751df0f97f5d4113aed82b5a3d5dd391e777ff
wafforyu <wafforyu@gmail.com>
2022-01-22 19:44:21 +0800

    allow passing of arguments

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import geocode from "./utils/geocode.js";
 import forecast from "./utils/forecast.js";
 
-geocode("Quezon City Philippines", (error, { lat, lon, loc }) => {
+geocode(process.argv[2], (error, { lat, lon, loc }) => {
   console.log(error ?? lat, lon, loc);
   forecast(lat, lon, (error, forecastData) => {
     console.log(error ?? `Location: ${loc}\n${forecastData}`);
